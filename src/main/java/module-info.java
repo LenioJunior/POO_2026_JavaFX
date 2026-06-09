fb49be6c1310bade0ee0f3d72a6416ab02611bf6
Lênio Prado <lenio.prado@ifsuldeminas.edu.br>
2026-06-09 14:59:03 -0300

    feat: extend module-info.java for Spring Boot and JPA

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,17 @@
-module com.example {
+module br.edu.ifsuldeminas {
     requires javafx.controls;
     requires javafx.fxml;
+    requires spring.context;
+    requires spring.beans;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.data.jpa;
+    requires spring.tx;
+    requires jakarta.persistence;
 
-    opens com.example to javafx.fxml;
-    exports com.example;
+    opens br.edu.ifsuldeminas to javafx.fxml, spring.core, spring.beans, spring.context;
+    opens br.edu.ifsuldeminas.models to jakarta.persistence, spring.core, org.hibernate.orm.core;
+    opens br.edu.ifsuldeminas.repositories to spring.core, spring.data.jpa;
+
+    exports br.edu.ifsuldeminas;
 }
